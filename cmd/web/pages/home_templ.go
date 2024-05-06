92952192e983d6aa5fa62a5f27f91a281bdbac76
Daniel Newman <contact@lifeofdan.me>
2024-05-06 12:15:49 +1000

    Remove hacky comment from tailwindcss testing

--- cmd/web/pages/home_templ.go
+++ cmd/web/pages/home_templ.go
@@ -12,7 +12,6 @@ import "bytes"
 
 import "github.com/lifeofdan/quickstart/cmd/web/layouts"
 
-// bg-blue-900
 func Home() templ.Component {
 	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
